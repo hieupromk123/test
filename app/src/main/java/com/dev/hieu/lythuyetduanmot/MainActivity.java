08a17cd4ae2da47c628b57e80e7d2710fa3bfa52
OngDat <metlam446@gmail.com>
2018-10-30 13:33:32 +0700

    add Log

--- app/src/main/java/com/dev/hieu/lythuyetduanmot/MainActivity.java
+++ app/src/main/java/com/dev/hieu/lythuyetduanmot/MainActivity.java
@@ -2,6 +2,7 @@ package com.dev.hieu.lythuyetduanmot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        Log.e("dada","daada");
     }
 }
